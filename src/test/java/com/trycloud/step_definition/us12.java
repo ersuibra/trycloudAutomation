950737a270ef25f606713d0a8734a09a1b125303
Selda Kartal <seldakartal51@gmail.com>
2021-10-19 01:50:56 -0400

    some updates are made

--- src/test/java/com/trycloud/step_definition/us12.java
+++ src/test/java/com/trycloud/step_definition/us12.java
@@ -5,7 +5,6 @@ import io.cucumber.java.en.Then;
 public class us12 {
     @Then("verify the contact names are in the list")
     public void verify_the_contact_names_are_in_the_list() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 }
