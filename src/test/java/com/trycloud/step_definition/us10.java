decd922a082c9eba63ca7a0df36e69f5983e208a
HydroSurg <ileu009@gmail.com>
2021-10-23 17:56:00 -0400

    User story 9 and 10 completed

--- src/test/java/com/trycloud/step_definition/us10.java
+++ src/test/java/com/trycloud/step_definition/us10.java
@@ -1,31 +1,50 @@
 package com.trycloud.step_definition;
 
+import com.trycloud.pages.FilesPage;
+import com.trycloud.utilities.BrowserUtil;
+import com.trycloud.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
 
 public class us10 {
+    FilesPage filesPage = new FilesPage();
+    String storage = filesPage.storageUsage.getText();
     @When("click Settings on the left bottom corner")
     public void click_settings_on_the_left_bottom_corner() {
-
+        filesPage.settingsButton.click();
     }
 
     @Then("verify user can click any buttons.")
     public void verify_user_can_click_any_buttons() {
-
+        for (WebElement checkBox : filesPage.checkBoxes) {
+            boolean beforeClick = checkBox.isSelected();
+            checkBox.click();
+            boolean afterClick = checkBox.isSelected();
+            Assert.assertNotEquals(!beforeClick, afterClick);
+        }
     }
 
     @When("check the current storage usage")
     public void check_the_current_storage_usage() {
-
+        storage= storage.substring(0,(storage.indexOf(" ")));
+        System.out.println("Current storage = " + storage);
     }
 
     @When("refresh the page")
     public void refresh_the_page() {
+        Driver.getDriver().navigate().refresh();
 
     }
 
     @Then("verify the storage usage is increased")
     public void verify_the_storage_usage_is_increased() {
+        String beforeUpload = storage;
+        BrowserUtil.waitFor(10);
+        String afterUpload = storage;
+
+        Assert.assertTrue(Integer.parseInt(beforeUpload)!=Integer.parseInt(afterUpload));
 
     }
 
