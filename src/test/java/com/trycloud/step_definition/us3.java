5a4b84a623a39641a991276205279cad6d688081
Ibrahim Ersu <86684993+ersuibra@users.noreply.github.com>
2021-10-23 22:33:11 -0400

    Update us3.java

--- src/test/java/com/trycloud/step_definition/us3.java
+++ src/test/java/com/trycloud/step_definition/us3.java
@@ -1,7 +1,7 @@
 package com.trycloud.step_definition;
 
 import com.trycloud.pages.LoginPage;
-import com.trycloud.pages.US3_Page_FilesModule;
+import com.trycloud.pages.FilesModulePage;
 import com.trycloud.pages.DashboardPage;
 import com.trycloud.pages.FilesPage;
 import com.trycloud.utilities.BrowserUtil;
@@ -14,12 +14,12 @@ import java.util.List;
 
 public class us3 {
 
-    US3_Page_FilesModule us3_page_filesModule = new US3_Page_FilesModule();
+    FilesModulePage filesModule = new FilesModulePage();
 
     @Then("verify the page tile is “Files - Trycloud”")
     public void verify_the_page_tile_is_files_trycloud() {
 
-        us3_page_filesModule.files.click();
+        filesModule.files.click();
         BrowserUtil.waitFor(1);
         String currentTitle = Driver.getDriver().getTitle();
         System.out.println("currentTitle = " + currentTitle);
@@ -32,9 +32,9 @@ public class us3 {
 
     @When("user click the top left checkbox of the table")
     public void user_click_the_top_left_checkbox_of_the_table() {
-        us3_page_filesModule.files.click();
+        filesModule.files.click();
         BrowserUtil.waitFor(1);
-        us3_page_filesModule.topLeftCheckBox.click();
+        filesModule.topLeftCheckBox.click();
 
     }
 
@@ -42,7 +42,7 @@ public class us3 {
     public void assert_all_the_files_are_selected() {
 
         BrowserUtil.waitFor(1);
-        Assert.assertTrue(BrowserUtil.checkAllBoxVerify(us3_page_filesModule.allCheckBoxes));
+        Assert.assertTrue(filesModule.checkAllBoxVerify(filesModule.allCheckBoxes));
         ///
 
     }
