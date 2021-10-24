8121cb17d8dda7fd22db7de19dc0e00f6a6d7e06
Ibrahim Ersu <86684993+ersuibra@users.noreply.github.com>
2021-10-24 00:47:20 -0400

    Merge pull request from ersuibra/Selda_new_banch

--- src/test/java/com/trycloud/step_definition/us1_LoginStepDefs.java
+++ src/test/java/com/trycloud/step_definition/us1_LoginStepDefs.java
@@ -9,20 +9,19 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 
-public class us1 {
+public class us1_LoginStepDefs {
     LoginPage loginPage = new LoginPage();
 
     @Given("user on the login page")
     public void user_on_the_login_page() {
         Driver.getDriver().get(ConfigReader.read("url"));
 
-
     }
 
-    @When("user enter valid username and password")
-    public void user_enter_valid_username_and_password() {
-    loginPage.userName.sendKeys(ConfigReader.read("user1"));
-    loginPage.password.sendKeys(ConfigReader.read("password"));
+    @When("user enter valid {string} and {string}")
+    public void user_enter_valid_and(String username, String password) {
+        loginPage.login(username, password);
+
     }
 
     @When("user click login button")
@@ -32,24 +31,24 @@ public class us1 {
 
     @Then("verify user launched to the dashboard")
     public void verify_user_launched_to_the_dashboard() {
-        Assert.assertTrue( Driver.getDriver().getTitle().startsWith("Dashboard"));
+        Assert.assertEquals("Dashboard - Trycloud", Driver.getDriver().getTitle());
     }
 
     @When("user enter invalid credentials")
     public void user_enter_invalid_credentials() {
-        loginPage.userName.sendKeys(ConfigReader.read("wuser1"));
-        loginPage.password.sendKeys(ConfigReader.read("wpassword"));
+        loginPage.usernameBox.sendKeys(ConfigReader.read("wuser1"));
+        loginPage.passwordBox.sendKeys(ConfigReader.read("wpassword"));
     }
 
-    @Then("“Wrong username or password.” message should be displayed")
-    public void wrong_username_or_password_message_should_be_displayed() {
-        Assert.assertEquals("Wrong username or password.", loginPage.errorMessage.getText());
+    @Then("{string} message should be displayed")
+    public void wrong_username_or_password_message_should_be_displayed(String message) {
+        Assert.assertEquals(message, loginPage.warningMessage.getText());
     }
 
     @When("user use username {string} and passcode {string}")
     public void user_use_username_and_passcode(String username, String password) {
-        loginPage.userName.sendKeys(username);
-        loginPage.password.sendKeys(password);
+        loginPage.usernameBox.sendKeys(username);
+        loginPage.passwordBox.sendKeys(password);
     }
 
 
