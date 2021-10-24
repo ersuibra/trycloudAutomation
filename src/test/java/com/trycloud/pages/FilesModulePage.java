f5bbea7bc74e8d38246b50b302c69bd7d643da23
Ibrahim Ersu <86684993+ersuibra@users.noreply.github.com>
2021-10-23 22:29:16 -0400

    Update and rename US3_Page_FilesModule.java to FilesModulePage.java

--- src/test/java/com/trycloud/pages/FilesModulePage.java
+++ src/test/java/com/trycloud/pages/FilesModulePage.java
@@ -8,32 +8,25 @@ import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
-public class US3_Page_FilesModule {
+public class FilesModulePage {
 
-    public US3_Page_FilesModule() {
+    public FilesModulePage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
     @FindBy(xpath = "//*[@id='appmenu']/li[2]/a")    // (//ul/li/a[@class='active'])[1]         (//ul/li[@data-id='files'])[1]
     public WebElement files;
 
-
-
     @FindBy(css = "#headerSelection > label")     //   id = "select_all_files"   //
     public WebElement topLeftCheckBox;
-
-
 
     @FindBy(xpath = "//td[@class='selection']/input")     //      //td/input[@type='checkbox']     td/input[@class='selectCheckBox checkbox']
     public List<WebElement> allCheckBoxes;            //td[@class='selection']
 
-
     @FindBy(xpath = "(//td/label/span[@class='hidden-visually'])[5]")
     public WebElement oneCheckBox;
 
-
     public boolean checkAllBoxVerify(List<WebElement> allBoxes){
-
         for (WebElement eachBox : allBoxes) {
             if(!eachBox.isSelected()){
                 return false;
