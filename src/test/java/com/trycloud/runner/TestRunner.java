161e129e54b3e2e52dd78aa63ee931a3fa44ff97
HydroSurg <ileu009@gmail.com>
2021-10-30 11:40:02 -0400

    updated runner class

--- src/test/java/com/trycloud/runner/TestRunner.java
+++ src/test/java/com/trycloud/runner/TestRunner.java
@@ -10,12 +10,12 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
 
         features = "src/test/resources/features",
-        glue = "com/trycloud/step_definitions",
+        glue = "com/trycloud/step_definition",
         publish = false,
         dryRun = false,
         plugin = {"pretty", "html:target/cucumber_report1.html"
                 , "rerun:target/rerun.txt", "me.jvt.cucumber.report.PrettyReports:target"
-        }, tags = "@ui"
+        } //tags = "@ui"
         //@wip
 
 )
